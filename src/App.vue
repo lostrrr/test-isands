64107a250aaf452031e7187f53945c323733883a
Lost <lostnxt@gmail.com>
2023-04-09 13:48:11 +0700

    add initial files

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <AppHeader />
+</template>
+
+<script>
+import AppHeader from "./components/AppHeader.vue";
+
+export default {
+  name: "App",
+  components: {
+    AppHeader,
+  },
+};
+</script>
+
+<style></style>
